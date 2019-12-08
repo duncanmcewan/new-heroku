b0c3aa5a86d84bd4e7cca46cc61cbae7beb2cd27
duncanmcewan <54848585+duncanmcewan@users.noreply.github.com>
2019-12-08 18:47:31 +0000

    Add Mongoose conenction options back in

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(express.static("public"));
 //mongoose.connect('mongodb+srv://duncan:test123@cluster0-kuucd.mongodb.net/xyz?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true});
 //mongoose.connect('mongodb+srv://duncan:test123@cluster0-kuucd.mongodb.net/test?retryWrites=true&w=majority',{dbName: "blog", useNewUrlParser: true, useUnifiedTopology: true});
 //mongoose.connect('mongodb+srv://duncan:test123@cluster0-kuucd.mongodb.net/blog',{useNewUrlParser: true, useUnifiedTopology: true});
-//mongoose.connect('mongodb+srv://duncanmcewan:test123@cluster0-w7pho.mongodb.net/blog?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect('mongodb+srv://duncanmcewan:test123@cluster0-w7pho.mongodb.net/blog?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://duncanmcewan:test123@cluster0-w7pho.mongodb.net/blog?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect('mongodb+srv://duncanmcewan:test123@cluster0-w7pho.mongodb.net/blog?retryWrites=true&w=majority');
 const blogSchema = new mongoose.Schema({
 //  _id: Number,
   title: String,
